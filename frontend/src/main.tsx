026e438b744a5c2ae3f73b8dbcda03c3c2214a8f
PARKOHSAM <ajaoadediran@gmail.com>
2026-09-17 22:58:45 +0100

    Fix frontend build and dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
 import { QueryClientProvider } from '@tanstack/react-query'
